dd62d61d8ee21a03b5e5ae622c32ed349d859255
Tomas Matthesius <tomas.matthesius@gmail.com>
2026-06-12 13:18:35 +0200

    test login.php

---
+++ LogInPage/login.php
@@ -0,0 +1,40 @@
+<?php
+
+require 'DB.php';
+
+session_start();
+
+if (isset($_SESSION['loggedInUser'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$naam = null;
+$wachtwoord = null;
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+    
+    
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
+    
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    
+        $stmt->execute();
+    
+        $user = $stmt->fetch();
+    
+    if ($user !== false) {
+        $_SESSION['loggedInUser'] = $user['id'];
+        unset($_SESSION['error']);
+        header("Location: index.php");
+        exit;
+    } else {
+        $_SESSION['error'] = "fout wachtwoord of gebruiker";
+        header("Location: login.html");
+    }
+}
+
+?>
