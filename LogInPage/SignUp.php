ef4d85c0df4148811983b5d7f3b995d142ff5115
Rafael Kes <rafaelmckes@gmail.com>
2026-06-17 10:59:12 +0200

    Added Sign up and href to login and sign up respetively

--- LogInPage/SignUp.php
+++ LogInPage/SignUp.php
@@ -10,38 +10,38 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Trivial</title>
-    <link rel="stylesheet" href="login.css">
+    <title>Sign Up - Trivial</title>
+    <link rel="stylesheet" href="SignUp.css">
 </head>
 
 <body>
 
     <div class="login-container">
         <div class="login-card">
-            <h1>Trivial</h1>
-            <p>Log in to save your scores.</p>
+            <h1>Sign up</h1>
+            <p>Create your trivial account!.</p>
 
             <form method="post" action="login_backend.php" id="loginForm">
                 <div class="input-group">
                     <label>Username</label>
-                    <input type="text" id="username" name="username" required>
+                    <input type="text" id="username" name="username" placeholder="Name" required>
                 </div>
                 <div class="input-group">
                     <label>E-mail</label>
-                    <input type="email" id="email" name="email" required>
+                    <input type="email" id="email" name="email"placeholder="name@email.com" required>
                 </div>
 
                 <div class="input-group">
                     <label>Password</label>
-                    <input type="password" id="password" name="password" required>
+                    <input type="password" id="password" name="password" placeholder="don't make your password too simple!" required>
                 </div>
 
                 <?php if (isset($_SESSION['loginError'])) { ?>
                     <span style="color : red"> <?= $_SESSION['loginError'] ?> </span> <br>
                 <?php } ?>
 
-                <a href="../LogInPage/SignUp.php">Don't have an account yet? Sign up here</a>
-                <button type="submit">Log in</button>
+                <a href="../LogInPage/login.php">Already have an account? Sign in instead here</a>
+                <button  class="signb" type="submit">Sign up</button>
 
             </form>
             <div class="links"></div>
