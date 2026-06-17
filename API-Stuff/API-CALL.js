15b2275d6011bb0b278ac371e646b8e07d224858
Hugo Jurriaans <jurriaans.hugo@gmail.com>
2026-06-17 10:21:45 +0200

    Coded API structure needs further development on backend

--- API-Stuff/API-CALL.js
+++ API-Stuff/API-CALL.js
@@ -1,42 +1,51 @@
-const GetCompiledAPI = class GetCompiledAPI {
+class GetCompiledAPI {
     constructor(id, name) {
         this.id = id;
         this.name = name;
     };
 
     async getAllQuizzes() {
-        const allquiz = [];
-        const quizresponse = await fetch("../API-Stuff/quizzesAPI.php?id=1");
-        //const questionresponse = await fetch();
-        //const answerresponse = await fetch();
-
-        const quizdata = await quizresponse.json();
-        //const questiondata = await questionresponse.json();
-        //const answerdata = await answerresponse.json();
-        return quizdata;
+        const res = await fetch(`wholequizAPI.php`); // needs proper endpoint //
+        const data = res.json();
+        return data;
     }
 
-    getQuizByFilter() {
-
+    async getQuizById() {
+        const res = await fetch(`wholequizAPI.php?quiz_id=${this.id}`); // Works! //
+        const data = res.json();
+        return data;
     }
 
-    getAllUsers() {
-
+    async getQuizByName() {
+        const res = await fetch(`wholequizAPI.php?title=${this.id}`); // Needs proper Endpoint //
+        const data = res.json();
+        return data;
     }
 
-    getTenUsers() {
-
+    async getAllUsers() {
+        const res = await fetch(`usersAPI.php`); // Works! //
+        const data = res.json();
+        return data;
     }
 
-    getUserByFilter() {
+    async getLeaderboardbyId() {
+        const res = await fetch(`leaderboardAPI.PHP?quiz_id=${this.id}`);
+        const data = res.json();
+        return data;
+    }
 
+    async getUserByName() {
+        const res = await fetch(`usersAPI.php?username=${this.name}`); // Works! //
+        const data = res.json();
+        return data;
     }
 }
 
 async function call() {
-    const test = await new GetCompiledAPI().getAllQuizzes();
+    const test = await new GetCompiledAPI(2, "arne").getQuizByName();
     console.log(test);
 }
 
 call();
-//export default { GetCompiledAPI };
+
+export default GetCompiledAPI;
--- API-Stuff/APITEST.js
+++ API-Stuff/APITEST.js
@@ -1,24 +1,4 @@
-//fetch("../API-Stuff/attempt_answersAPI.php?id=1")
-//.then((data) => (data.json()))
-//.then((data) => console.log(data));
-//
-//fetch("../API-Stuff/attemptsAPI.php?id=1")
-//.then((data) => (data.json()))
-//.then((data) => console.log(data));
-//
-//fetch("../API-Stuff/usersAPI.php?username=arne")
-//.then((data) => (data.json()))
-//.then((data) => console.log(data));
-
-fetch("../API-Stuff/optionsAPI.php?id=5")
-.then((data) => (data.json()))
-.then((data) => console.log(data));
-
-fetch("../API-Stuff/questionsAPI.php?id=1")
-.then((data) => (data.json()))
-.then((data) => console.log(data));
-
-fetch("../API-Stuff/quizzesAPI.php?id=1")
+fetch("leaderboardAPI.php")
 .then((data) => (data.json()))
 .then((data) => console.log(data));
 
