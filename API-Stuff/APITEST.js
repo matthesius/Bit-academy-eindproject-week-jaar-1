e3cc03cd498c6b677f0d67a91848b75d72820d19
Arne Veltman <a.veltman@student.talland.nl>
2026-06-18 11:27:34 +0200

    apitesting

--- API-Stuff/APITEST.js
+++ API-Stuff/APITEST.js
@@ -1,4 +1,8 @@
-fetch("leaderboardAPI.php")
+fetch("../API-Stuff/apithingie.php?action=getQuizAttempts&quiz_id=1")
+.then((data) => (data.json()))
+.then((data) => console.log(data));
+
+fetch("../API-Stuff/apithingie.php?action=getUserAttempts&user_id=1")
 .then((data) => (data.json()))
 .then((data) => console.log(data));
 
