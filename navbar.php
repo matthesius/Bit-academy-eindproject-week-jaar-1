44f7a09bea38dc027ec0e7bbf6a402b4ee84da5e
Tomas Matthesius <tomas.matthesius@gmail.com>
2026-06-18 13:36:21 +0200

    navbar styling WIP

--- navbar.php
+++ navbar.php
@@ -1,21 +1,31 @@
-<nav>
+<!DOCTYPE html>
+<html>
+<head>
     <link rel="stylesheet" href="navbar.css">
+</head>
+<body>
+
+<nav>
     <div class="logo">Trivial</div>
 
     <div class="nav-links">
         <a href="../HomePage/homepage.php">Home</a>
-        <a href="../overview/index.php">Quizzen</a>
-        <a href="../Leaderboard/leaderboard.php" class="active">Leaderboard</a>
+        <a href="../overview/index.html">Quizzen</a>
+        <a href="../Leaderboard/leaderboard.php">Leaderboard</a>
 
         <?php
         if (session_status() !== PHP_SESSION_ACTIVE) {
-            session_start();    
+            session_start();
         }
 
         if (isset($_SESSION['LoggedInQuizTaker'])) { ?>
             <a href="../LogInPage/logout.php">Log out</a>
+            <a href="../userpage/userpage.php">User</a>
         <?php } else { ?>
             <a href="../LogInPage/login.php">Log in</a>
         <?php } ?>
     </div>
 </nav>
+
+</body>
+</html>
