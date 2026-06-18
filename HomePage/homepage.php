f37c249ec7944a7115abeb298f6c17699f8f5f3c
Rafael Kes <rafaelmckes@gmail.com>
2026-06-18 10:09:57 +0200

    updated stuff

--- HomePage/homepage.php
+++ HomePage/homepage.php
@@ -6,13 +6,15 @@ require_once '../LogInPage/login_check.php';
 
 <!DOCTYPE html>
 <html lang="nl">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trivial</title>
     <link rel="stylesheet" href="homepage.css">
-        <link rel="icon" type="image/png" href="../favicon/web-app-manifest-192x192.png" />
+    <link rel="icon" type="image/png" href="../favicon/web-app-manifest-192x192.png" />
 </head>
+
 <body>
 
     <nav>
@@ -21,8 +23,9 @@ require_once '../LogInPage/login_check.php';
         <div class="nav-links">
             <a href="homepage.php">Home</a>
             <a href="/Bit-academy-project-week-1/overview/index.php">Quizzen</a>
-    <a href="../LogInPage/login.php" class="signup-btn">Sign Up</a>
-</div>
+            <a href="/Bit-academy-project-week-1/Leaderboard/leaderboard.php">Leaderboard</a>
+            <a href="../LogInPage/login.php" class="signup-btn">Sign Up</a>
+        </div>
     </nav>
 
     <section class="hero">
@@ -75,4 +78,5 @@ require_once '../LogInPage/login_check.php';
         © 2026 Trivial
     </footer>
 </body>
+
 </html>
