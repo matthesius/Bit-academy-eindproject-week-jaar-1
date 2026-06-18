fbf03bff9e89a66db53be32ecc4cc6801bbfd6c4
Rafael Kes <rafaelmckes@gmail.com>
2026-06-18 10:07:55 +0200

    added seperate navbar elements

---
+++ navbar.php
@@ -0,0 +1,23 @@
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Navbar</title>
+        <link rel="stylesheet" href="navbar.css">
+    </head>
+
+    <body>
+        <nav>
+            <div class="logo">Trivial</div>
+
+            <div class="nav-links">
+                <a href="..//HomePage/homepage.php">Home</a>
+                <a href="/Bit-academy-project-week-1/overview/index.php">Quizzen</a>
+                <a href="leaderboard.php" class="active">Leaderboard</a>
+            </div>
+        </nav>
+    </body>
+
+    </html>
