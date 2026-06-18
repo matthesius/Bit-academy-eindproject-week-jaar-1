3878985148ca74ea3e8044d40048f61bee767168
Tomas Matthesius <tomas.matthesius@gmail.com>
2026-06-18 12:29:34 +0200

    session status check

--- navbar.php
+++ navbar.php
@@ -8,7 +8,9 @@
         <a href="../Leaderboard/leaderboard.php" class="active">Leaderboard</a>
 
         <?php
-        session_start();
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            session_start();    
+        }
 
         if (isset($_SESSION['LoggedInQuizTaker'])) { ?>
             <a href="../LogInPage/logout.php">Log out</a>
