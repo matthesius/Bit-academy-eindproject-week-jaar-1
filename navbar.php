def8f4d2563febb8b5b6192c680579b89d72b757
Tomas Matthesius <tomas.matthesius@gmail.com>
2026-06-18 10:19:13 +0200

    updated navbar logic

--- navbar.php
+++ navbar.php
@@ -1,23 +1,10 @@
-    <!DOCTYPE html>
-    <html lang="en">
+<nav>
+    <link rel="stylesheet" href="navbar.css">
+    <div class="logo">Trivial</div>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Navbar</title>
-        <link rel="stylesheet" href="navbar.css">
-    </head>
-
-    <body>
-        <nav>
-            <div class="logo">Trivial</div>
-
-            <div class="nav-links">
-                <a href="..//HomePage/homepage.php">Home</a>
-                <a href="/Bit-academy-project-week-1/overview/index.php">Quizzen</a>
-                <a href="leaderboard.php" class="active">Leaderboard</a>
-            </div>
-        </nav>
-    </body>
-
-    </html>
+    <div class="nav-links">
+        <a href="../HomePage/homepage.php">Home</a>
+        <a href="../overview/index.php">Quizzen</a>
+        <a href="../Leaderboard/leaderboard.php" class="active">Leaderboard</a>
+    </div>
+</nav>
