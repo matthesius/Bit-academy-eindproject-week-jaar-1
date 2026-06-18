48ca05c850829d8ad71b1940acf1c3a0ec2986de
Tomas Matthesius <tomas.matthesius@gmail.com>
2026-06-18 15:00:03 +0200

    arnefix

--- overview/index.php
+++ overview/index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz Overzicht</title>
     <link href="style.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="../favicon/web-app-manifest-192x192.png" />
+    <!-- to do: verander deze kut slop favicon AUB -->
 </head>
 <body>
 
