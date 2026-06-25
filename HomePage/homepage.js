7bc5aa68834e925302003ccc98f856a30eb69304
Hugo Jurriaans <jurriaans.hugo@gmail.com>
2026-06-25 12:37:37 +0200

    fixed homepage popular quizzes

--- HomePage/homepage.js
+++ HomePage/homepage.js
@@ -1,36 +1,36 @@
 console.log(quizzes);
+const quizContainer = document.getElementById("featuredContainer");
 const array = 
 [quizzes.quiz_1, quizzes.quiz_2, quizzes.quiz_3, quizzes.quiz_4, quizzes.quiz_5];
 
 async function renderPopQuizzes() {
-
+    quizContainer.innerHTML = "";
     for (let i = 0; i < array.length; i++) {
         const res = await fetch(`../API-Stuff/apithingie.php?action=getQuiz&quiz_id=${array[i]}`);
-        const data = await res.json();
-        console.log(data);
-        quizContainer.innerHTML = "";
-
+        const quiz = await res.json();
+        console.log(quiz);
+        
         const card = document.createElement("div");
         card.className = "quiz-card";
 
         const img = document.createElement("img");
         img.className = "quiz-image";
-        img.src = quiz[i].image_url;
-        img.alt = quiz[i].title;
+        img.src = quiz.image_url;
+        img.alt = quiz.title;
 
         const content = document.createElement("div");
         content.className = "quiz-content";
 
         const title = document.createElement("h2");
-        title.textContent = quiz[i].title;
+        title.textContent = quiz.title;
 
         const desc = document.createElement("p");
         desc.className = "quiz-description";
-        desc.textContent = quiz[i].description ?? "Geen beschrijving";
+        desc.textContent = quiz.description ?? "Geen beschrijving";
 
         const stats = document.createElement("div");
         stats.className = "quiz-stats";
-        stats.textContent =  `${quiz[i].question_count} vragen` //`${quiz[i].questions.length} vragen`; //
+        stats.textContent =  `${quiz.questions.length} vragen` //`${quiz[i].questions.length} vragen`; //
         
         const button = document.createElement("a");
         button.href = "../quiz/quiz.php";
@@ -38,7 +38,7 @@ async function renderPopQuizzes() {
         button.className = "start-btn";
 
         button.addEventListener("click", () => {
-            startQuiz(quiz[i].id);
+            startQuiz(quiz.id);
         });
         
         content.appendChild(title);
