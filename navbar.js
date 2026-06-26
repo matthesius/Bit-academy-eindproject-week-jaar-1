5bacc9299ad55a7f386aa725e85e425b8a412311
Hugo Jurriaans <jurriaans.hugo@gmail.com>
2026-06-26 14:36:26 +0200

    fix popular quizzes on homepage is diabled

--- navbar.js
+++ navbar.js
@@ -1,7 +1,6 @@
 const opennavmodal = document.getElementById("responsivemenu");
 const closenavmodal = document.getElementById("closenavmodal");
 const navmodal = document.getElementById("navmodal");
-const modallinks = document.querySelectorAll(".modal > a");
 
 navmodal.style.display = "none";
 
@@ -10,9 +9,5 @@ opennavmodal.addEventListener("click", () => {
 });
 
 closenavmodal.addEventListener("click", () => {
-    navmodal.style.display = "none";
-});
-
-modallinks.addEventListener("click", () => {
     navmodal.style.display = "none";
 });
