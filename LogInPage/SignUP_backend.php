dabb0b26b73a6c206aaee21f900d26122e160972
Tomas Matthesius <tomas.matthesius@gmail.com>
2026-07-01 11:58:42 +0200

    login throttling toegevoegd

--- LogInPage/SignUP_backend.php
+++ LogInPage/SignUP_backend.php
@@ -15,29 +15,32 @@ $password1;
 $password2;
 
 if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirmPassword'])) {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password1 = $_POST['password'];
-    $password2 = $_POST['confirmPassword'];
-
-    $stmt = $pdo->prepare("SELECT username, email FROM users WHERE username = :username OR email = :email");
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-    $stmt->execute();
-    $user_check = $stmt->fetchall();
-
     try {
         if (filter_var("$username", FILTER_VALIDATE_EMAIL)) {
             throw new Exception("email in gebruikersnaam veld" . PHP_EOL);
         }
-        if ($password1 !== $password2) {
+        if ($password1 !== $password2) {                                    //checkt eerst de simpele eisen voor de sql query voor potentieel minder database lag
             throw new Exception("wachtwoorden zijn verschillend" . PHP_EOL);
         }
+
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password1 = $_POST['password'];
+        $password2 = $_POST['confirmPassword'];
+
+        $stmt = $pdo->prepare("SELECT username, email FROM users WHERE username = :username OR email = :email");
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        $user_check = $stmt->fetchall();
+
         if (!empty($user_check)) {
             throw new Exception("gebruikersnaam of email is al in gebruik" . PHP_EOL);
         }
-        unset($_SESSION['SignUpError']);
+        unset($_SESSION['SignUpError']);        //verwijder alle errors
         unset($_SESSION['loginError']);
+        unset($_SESSION['failed_logins']);
+        unset($_SESSION['sleep_until']);
         $hashed_password = password_hash("$password1", PASSWORD_DEFAULT);
 
         $stmt = $pdo->prepare("INSERT INTO users (username, password, email) VALUES (:username, :password, :email);");
--- LogInPage/login_backend.php
+++ LogInPage/login_backend.php
@@ -9,8 +9,69 @@ if (isset($_SESSION['LoggedInQuizTaker'])) {
     exit;
 }
 
+if (isset($_SESSION['sleep_until'])) {      //blocks login attempts when timed out
+    $current_time = explode(" ", microtime());
+    $exploded_target_time = explode(" ", $_SESSION['sleep_until']);
+    $time_until_retry = $exploded_target_time[1] - $current_time[1];
+    if ($time_until_retry > 0) {
+        $_SESSION['loginError'] = "you are being rate limited. try again in $time_until_retry seconds";
+        header("Location: login.php");
+        exit;
+    } else {
+        unset($_SESSION['sleep_until']);
+    }
+}
+
 $naam = null;
 $wachtwoord = null;
+
+function login_throttling() //stopt login attempt spam
+{
+    $login_penalties = [30, 60, 180, 300, 600]; // 30s, 1m, 3m, 5m, 10m penalties based on login attemtps
+
+    $current_time = microtime();
+    $_SESSION['failed_logins'][] = $current_time;
+    $current_time = explode(" ", $current_time);
+
+    switch (count($_SESSION['failed_logins'])) {
+        case 3:
+            $target_time = "$current_time[0] " . $current_time[1] + $login_penalties[0];
+            $_SESSION['sleep_until'] = $target_time;
+            break;
+        case 6:
+            $target_time = "$current_time[0] " . $current_time[1] + $login_penalties[1];
+            $_SESSION['sleep_until'] = $target_time;
+            break;
+        case 9:
+            $target_time = "$current_time[0] " . $current_time[1] + $login_penalties[2];
+            $_SESSION['sleep_until'] = $target_time;
+            break;
+        case 15:
+            $target_time = "$current_time[0] " . $current_time[1] + $login_penalties[3];
+            $_SESSION['sleep_until'] = $target_time;
+            break;
+        case 20:
+            $target_time = "$current_time[0] " . $current_time[1] + $login_penalties[4];
+            $_SESSION['sleep_until'] = $target_time;
+            break;
+        case count($_SESSION['failed_logins']) > 20:
+            $target_time = "$current_time[0] " . $current_time[1] + $login_penalties[4];
+            $_SESSION['sleep_until'] = $target_time;
+            break;
+        default:
+            unset($_SESSION['sleep_until']);
+            $target_time = 0;
+    }
+
+    if ($target_time !== 0) {
+        $current_time = explode(" ", microtime());
+        $exploded_target_time = explode(" ", $target_time);
+        $time_until_retry = $exploded_target_time[1] - $current_time[1];
+        $_SESSION['loginError'] = "you are being rate limited. try again in $time_until_retry seconds";
+    }
+    header("Location: login.php");
+    exit;
+}
 
 if (isset($_POST['account']) && isset($_POST['password'])) {
         $account = $_POST['account'];
@@ -22,19 +83,23 @@ if (isset($_POST['account']) && isset($_POST['password'])) {
         $stmt->bindParam(':username', $account, PDO::PARAM_STR);
         $stmt->bindParam(':email', $account, PDO::PARAM_STR);
         $stmt->execute();
-        $user = $stmt->fetch();
+        $user = $stmt->fetch();                 //vult $user met password en id ($user['password'/'id']) als de email of gebruikers naam klopt
 
     if (password_verify("$password", $user['password'])) {
         $_SESSION['LoggedInQuizTaker'] = $user['id'];
-        unset($_SESSION['loginError']);
+        unset($_SESSION['loginError']);         //verwijder errors wanneer er successvol is ingelogd
         unset($_SESSION['SignUpError']);
+        unset($_SESSION['failed_logins']);
+        unset($_SESSION['sleep_until']);
         header("Location: ../HomePage/homepage.php");
         exit;
     } else {
         $_SESSION['loginError'] = "foute gebruikersnaam of wachtwoord";
+        login_throttling();
         header("Location: login.php");
+        exit;
     }
-} else {
+} else {                                            //als er missende velden zijn
     $_SESSION['loginError'] = "vul de velden in";
     header("Location: login.php");
     exit;
